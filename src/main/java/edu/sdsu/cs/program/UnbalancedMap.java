a1e458be8d1f262059528fdeee5e8e1904c11bf1
zKavtaradze <ZurabKavtaradze@protonmail.com>
2020-12-01 02:01:39 +0400

    delete functionality

--- src/main/java/edu/sdsu/cs/program/UnbalancedMap.java
+++ src/main/java/edu/sdsu/cs/program/UnbalancedMap.java
@@ -1,5 +1,6 @@
 package edu.sdsu.cs.program;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Stack;
 
@@ -10,7 +11,7 @@ public class UnbalancedMap<K extends Comparable<K>, V> implements IMap<K, V> {
     Node<K, V> root;
     int count = 0;
 
-    private class Node<K, V> {
+    private class Node<K extends Comparable<K>, V> {
         K key;
         V value;
         Node<K, V> left, right;
@@ -18,6 +19,55 @@ public class UnbalancedMap<K extends Comparable<K>, V> implements IMap<K, V> {
         public Node(K key, V value) {
             this.key = key;
             this.value = value;
+        }
+
+        public Node<K, V> getParentNode(Node<K, V> root) {
+            Node<K, V> parent = root;
+            int direction = 0;
+
+            while (parent.left != this && parent.right != this) {
+                direction = this.key.compareTo(parent.key);
+                if (direction < 0) {
+                    parent = parent.left;
+                } else if (direction > 0) {
+                    parent = parent.right;
+                }
+            }
+            return parent;
+        }
+
+        public Node<K, V> getChildWithKey(K key) {
+            if (this.hasLeftChild() && this.left.key.equals(key)) {
+                return this.left;
+            } else if (this.hasRightChild() && this.right.key.equals(key)) {
+                return this.right;
+            } else {
+                return null;
+            }
+        }
+
+        private Node<K, V> smallest() {
+            Node<K, V> smallest = this;
+            while (smallest.hasLeftChild()) {
+                smallest = smallest.left;
+            }
+            return smallest;
+        }
+
+        public Node<K, V> inOrderPredecessor() {
+            Node<K, V> predecessor = this;
+            if (predecessor.hasRightChild()) {
+                return predecessor.right.smallest();
+            }
+            return null;
+        }
+
+        public boolean hasLeftChild() {
+            return left != null;
+        }
+
+        public boolean hasRightChild() {
+            return right != null;
         }
 
         boolean contains(K k) {
@@ -110,11 +160,107 @@ public class UnbalancedMap<K extends Comparable<K>, V> implements IMap<K, V> {
         }
         return false;
     }
-    
+
     @Override
     public V delete(K key) {
-        // TODO: Implement
-        return null;
+        if (!this.contains(key)) {
+            return null;
+        }
+
+        if (isOnlyRoot(key)) {
+            V value = root.value;
+            root = null;
+            count--;
+            return value;
+        }
+
+        if (isRoot(key)) {
+            V value = root.value;
+            replaceRoot();
+            count--;
+            return value;
+
+        }
+
+        Node<K, V> parent = getParentNodeByKey(key);
+        Node<K, V> active = parent.getChildWithKey(key);
+
+        if (!active.hasLeftChild() && !active.hasRightChild()) {
+            if (parent.left == active) {
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        }
+
+        if (active.hasLeftChild() && !active.hasRightChild()) {
+            if (parent.left == active) {
+                parent.left = active.left;
+            } else {
+                parent.right = active.left;
+            }
+        } else if (!active.hasLeftChild() && active.hasRightChild()) {
+            if (parent.left == active) {
+                parent.left = active.right;
+            } else {
+                parent.right = active.right;
+            }
+        } else if (active.hasLeftChild() && active.hasRightChild()) {
+            Node<K, V> replacement = active.inOrderPredecessor();
+
+            if (parent.left == active) {
+                parent.left = replacement;
+            } else {
+                parent.right = replacement;
+            }
+            replacement.left = active.left;
+            replacement.right = active.right;
+
+        }
+
+        count--;
+        active.left = null;
+        active.right = null;
+        return active.value;
+    }
+
+    private Node<K, V> getParentNodeByKey(K key) {
+        Node<K, V> active = root;
+        Node<K, V> parent = root;
+        int direction = key.compareTo(active.key);
+
+        while (direction != 0) {
+            parent = active;
+            if (direction < 0) {
+                active = active.left;
+            } else if (direction > 0) {
+                active = active.right;
+            }
+            direction = key.compareTo(active.key);
+        }
+        return parent;
+    }
+
+    private boolean isRoot(K key) {
+        return (key.compareTo(root.key) == 0);
+    }
+
+    private boolean isOnlyRoot(K key) {
+        return (isRoot(key) && size() == 1);
+    }
+
+    private void replaceRoot() {
+
+        Node<K, V> replacement = root.inOrderPredecessor();
+        Node<K, V> parent = replacement.getParentNode(root);
+        if (parent.left == replacement) {
+            parent.left = null;
+        } else {
+            parent.right = null;
+        }
+        replacement.left = root.left;
+        replacement.right = root.right;
+        root = replacement;
     }
 
     @Override
@@ -175,7 +321,7 @@ public class UnbalancedMap<K extends Comparable<K>, V> implements IMap<K, V> {
 
         while (!stack.empty()) {
             node = stack.pop();
-            list.add(root.value);
+            list.add(node.value);
             if (node.left != null)
                 stack.push(node.left);
             if (node.right != null)
@@ -192,7 +338,24 @@ public class UnbalancedMap<K extends Comparable<K>, V> implements IMap<K, V> {
 
         while (!stack.empty()) {
             node = stack.pop();
-            list.add(root.key);
+            list.add(node.key);
+            if (node.left != null)
+                stack.push(node.left);
+            if (node.right != null)
+                stack.push(node.right);
+        }
+    }
+
+    private void getAll(Node<K, V> node, LinkedList<Node<K, V>> list) {
+        if (node == null) {
+            return;
+        }
+        Stack<Node> stack = new Stack<>();
+        stack.push(node);
+
+        while (!stack.empty()) {
+            node = stack.pop();
+            list.add(node);
             if (node.left != null)
                 stack.push(node.left);
             if (node.right != null)
